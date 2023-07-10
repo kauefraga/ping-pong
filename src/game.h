194c5ff7c9acdba0c888a686b065c3d5bddc1dbd
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-07-09 22:58:23 -0300

    :sparkles: feat: create a header file to include raylib and define constants

---
+++ src/game.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <raylib.h>
+
+/* ----- PALETTE -----
+ * OOD FEELING PALETTE
+ * Creator: XENO
+ * See: https://lospec.com/palette-list/odd-feeling
+*/
+namespace palette {
+  const Color ODD_PURPLE{ 144, 12, 63, 255 };
+}
