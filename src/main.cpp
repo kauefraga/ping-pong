5aeac6d89c85070e6fe7751fccb64a6b5d811402
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-07-12 22:40:14 -0300

    :sparkles: feat: add a background and improve data structures, collisions and game physics

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,9 @@
-#include <raylib.h>
-#include "entities.h"
+#include "game.h"
 
-const int WIDTH = 1000;
-const int HEIGHT = 700;
-const int TABLE_WIDTH = 900;
-const int TABLE_HEIGHT = 500;
+using namespace palette;
 
 int main() {
-  InitWindow(WIDTH, HEIGHT, "Ping Pong"); // Initialize the window and opengl context
+  InitWindow(screen::WIDTH, screen::HEIGHT, "Ping Pong"); // Initialize the window and opengl context
 
   Image icon = LoadImage("resources/icon.png"); // Load image in memory
 
@@ -15,122 +11,141 @@ int main() {
 
   UnloadImage(icon); // Free the image (optimization)
 
-  int first_score = 0;
-  int second_score = 0;
+  Texture background = LoadTexture("resources/background-pixel.png"); // Load the table
 
-  Vector2 first_racket{ (WIDTH / 2) - (TABLE_WIDTH / 2) + 20, HEIGHT / 2 };
-  Vector2 second_racket{ (WIDTH / 2) + (TABLE_WIDTH / 2) - 20, HEIGHT / 2 };
-  Vector2 ball_position{ WIDTH / 2, HEIGHT / 2 };
-  Vector2 ball_speed{ 5, 0 };
+  int left_score = 0;
+  int right_score = 0;
+
+  Paddle left{
+    (GetScreenWidth() / 2.0f) - (table::WIDTH / 2) + 20, // 20 is a padding
+    GetScreenHeight() / 2.0f
+  };
+
+  Paddle right{
+    (GetScreenWidth() / 2.0f) + (table::WIDTH / 2) - 20, // -20 is a padding
+    GetScreenHeight() / 2.0f
+  };
+
+  Ball ball{};
 
   SetTargetFPS(60);
 
   // If ESC is pressed the game closes
   while (!WindowShouldClose()) {
     /* --- UPDATE --- */
-    // first racket (wasd)
-    if (IsKeyDown(KEY_S)) {
-      bool first_racket_floor_limit = CheckCollisionRecs(
-        { first_racket.x, first_racket.y, 5, 50 }, { 50, TABLE_HEIGHT + 120 - 5, TABLE_WIDTH, 1 }
-      );
 
-      first_racket_floor_limit ? first_racket.y : first_racket.y += 5;
+    ball.x += ball.speed_x * GetFrameTime();
+    ball.y += ball.speed_y * GetFrameTime();
+
+    // ball limits
+    bool ball_ceil_limit = CheckCollisionCircleRec({ ball.x, ball.y },
+      ball.radius, { table::OFFSET_X, table::OFFSET_Y, table::WIDTH, 1 });
+
+    bool ball_floor_limit = CheckCollisionCircleRec({ ball.x, ball.y },
+      ball.radius, { table::OFFSET_X, table::HEIGHT + table::OFFSET_Y, table::WIDTH, 1 });
+
+    bool ball_left_limit = CheckCollisionCircleRec({ ball.x, ball.y },
+      ball.radius, { table::OFFSET_X, table::OFFSET_Y, 1, table::HEIGHT });
+
+    bool ball_right_limit = CheckCollisionCircleRec({ ball.x, ball.y },
+      ball.radius, { table::WIDTH + table::OFFSET_X, table::OFFSET_Y, 1, table::HEIGHT });
+
+    if (ball_ceil_limit) {
+      ball.speed_y *= -1;
+    }
+
+    if (ball_floor_limit) {
+      ball.speed_y *= -1;
+    }
+
+    if (ball_left_limit) {
+      ball.speed_x *= -1;
+      right_score += 1; // if the ball touches the left wall, right paddle points
+    }
+
+    if (ball_right_limit) {
+      ball.speed_x *= -1;
+      left_score += 1; // if the ball toucher the right wall, left paddle points
+    }
+
+    // left paddle (wasd)
+    if (IsKeyDown(KEY_S)) {
+      left.y += PADDLE_SPEED * GetFrameTime();
     }
 
     if (IsKeyDown(KEY_W)) {
-      bool first_racket_ceil_limit = CheckCollisionRecs(
-        { first_racket.x, first_racket.y, 5, 50 }, { 50, 120, TABLE_WIDTH, 1 }
-      );
-
-      first_racket_ceil_limit ? first_racket.y : first_racket.y -= 5;
+      left.y -= PADDLE_SPEED * GetFrameTime();
     }
 
-    // second racket (arrows)
+    // right paddle (arrows)
     if (IsKeyDown(KEY_DOWN)) {
-      bool second_racket_floor_limit = CheckCollisionRecs(
-        { second_racket.x, second_racket.y, 5, 50 }, { 50, TABLE_HEIGHT + 120 - 5, TABLE_WIDTH, 1 }
-      );
-
-      second_racket_floor_limit ? second_racket.y : second_racket.y += 5;
+      right.y += PADDLE_SPEED * GetFrameTime();
     }
 
     if (IsKeyDown(KEY_UP)) {
-      bool second_racket_ceil_limit = CheckCollisionRecs(
-        { second_racket.x, second_racket.y, 5, 50 }, { 50, 120, TABLE_WIDTH, 1 }
-      );
-
-      second_racket_ceil_limit ? second_racket.y : second_racket.y -= 5;
+      right.y -= PADDLE_SPEED * GetFrameTime();
     }
 
-    /* --- COLLISIONS RACKET-BALL --- */
-    // first racket-ball collision
-    bool first_racket_collision = CheckCollisionCircleRec(ball_position, 20, { first_racket.x, first_racket.y, 3, 50 });
-    if (first_racket_collision) {
-      ball_speed.x = 5;
-      ball_speed.y = (float)GetRandomValue(-2, 2);
+    /* --- COLLISIONS paddle-BALL --- */
+    // left paddle-ball collision
+    if (CheckCollisionCircleRec(
+      { ball.x, ball.y },
+      ball.radius,
+      { left.x, left.y, left.width, left.height }
+    )) {
+      if (ball.speed_x < 0) {
+        ball.speed_x *= -1.1f;
+        ball.speed_y = (ball.y - left.y);
+      }
     }
 
-    // second racket-ball collision
-    bool second_racket_collision = CheckCollisionCircleRec(ball_position, 20, { second_racket.x, second_racket.y, 3, 50 });
-    if (second_racket_collision) {
-      ball_speed.x = -5;
-      ball_speed.y = (float)GetRandomValue(-2, 2);
+    // right paddle-ball collision
+    if (CheckCollisionCircleRec(
+      { ball.x, ball.y },
+      ball.radius,
+      { right.x, right.y, right.width, right.height }
+    )) {
+      if (ball.speed_x < 0) {
+        ball.speed_x *= -1.1f;
+        ball.speed_y = (ball.y - right.y);
+      }
     }
-
-    // ball limits
-    bool ball_ceil_limit = CheckCollisionCircleRec(ball_position,
-      20, { 50, 120, TABLE_WIDTH, 1 });
-
-    bool ball_floor_limit = CheckCollisionCircleRec(ball_position,
-      20, { 50, TABLE_HEIGHT + 120, TABLE_WIDTH, 1 });
-
-    bool ball_left_limit = CheckCollisionCircleRec(ball_position,
-      20, { 50, 120, 1, TABLE_HEIGHT });
-
-    bool ball_right_limit = CheckCollisionCircleRec(ball_position,
-      20, { TABLE_WIDTH + 50, 120, 1, TABLE_HEIGHT });
-
-    if (ball_ceil_limit) ball_speed.y = 1;
-    if (ball_floor_limit) ball_speed.y = -1;
-    if (ball_left_limit) {
-      ball_speed.x = 5;
-      second_score += 1; // if the ball touches the left wall, second racket points
-    }
-    if (ball_right_limit) {
-      ball_speed.x = -5;
-      first_score += 1; // if the ball toucher the right wall, first racket points
-    }
-
-    ball_position.x += ball_speed.x;
-    ball_position.y += ball_speed.y;
 
     /* --- DRAW --- */
     BeginDrawing();
 
-    ClearBackground(BLACK); // Clear the screen before draw stuff
-
-    DrawText(TextFormat("%ix%i", first_score, second_score),
-      (WIDTH / 2) - (MeasureText("xxx", 30) / 2),
-      30,
-      30,
-      GREEN
-    );
+    // Clear the screen before start drawing
+    ClearBackground(RAYWHITE);
 
     // Table
-    DrawRectangle(50, 120, TABLE_WIDTH, TABLE_HEIGHT, PINK);
+    DrawTexture(background, 0, 0, WHITE);
 
-    // First racket
-    DrawRectangleV(first_racket, { 5, 50 }, RAYWHITE);
+    // Score board
+    DrawText(TextFormat("%ix%i", left_score, right_score),
+      (screen::WIDTH / 2) - (MeasureText("xxx", 50) / 2),
+      35,
+      50,
+      ODD_PURPLE
+    );
 
-    // Second racket
-    DrawRectangleV(second_racket, { 5, 50 }, RAYWHITE);
+    // left paddle
+    left.draw();
+
+    // right paddle
+    right.draw();
 
     // Ball
-    DrawCircleV(ball_position, 20, BLUE);
+    ball.draw();
+
+    DrawFPS(50, 50);
 
     EndDrawing();
   }
 
+  // Unload background texture
+  UnloadTexture(background);
+
+  // Close window and unload opengl context
   CloseWindow();
 
   return 0;
