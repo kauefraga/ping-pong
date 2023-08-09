9a39dadf56b4aac740e88513792625537e6fb71b
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-08-09 19:00:47 -0300

    :sparkles: feat: fix #1, add some constants to handle the positions, centralize paddle and ball initial Y, remove fps counter, bind a ball restart (r) and reset the ball speed X when it's higher than 2500

--- src/game.h
+++ src/game.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <raylib.h>
 
 /* ----- SCREEN ----- */
@@ -6,25 +7,27 @@ namespace screen {
   const int WIDTH = 1000;
   const int HEIGHT = 700;
 
-  // FPS limit vs. Vsync
+  // The distance between the top to score origin.
+  const int SCORE_OFFSET_Y = 80;
 }
 
 /* ----- TABLE ----- */
 namespace table {
-  const int WIDTH = 900;
-  const int HEIGHT = 500;
+  const int WIDTH = 920;
+  const int HEIGHT = 440;
 
-  // The distance between the top to table-top and left to table-left
+  // The distance between the top to table-top and left to table-left.
   const int OFFSET_X = 50;
-  const int OFFSET_Y = 145;
+  const int OFFSET_Y = 230;
 }
 
 /* ----- PADDLE ----- */
 const int PADDLE_SPEED = 500;
+const int PADDLE_PADDING = 30;
 
 struct Paddle {
   float x;
-  float y;
+  float y = (GetScreenHeight() / 2.0f) + (table::HEIGHT / 2.0f - table::OFFSET_Y / 2.0f) - 50; // 50 is the paddle height
   float width = 5;
   float height = 50;
 
@@ -36,18 +39,24 @@ struct Paddle {
 /* ----- BALL ----- */
 struct Ball {
   float x = GetScreenWidth() / 2.0f;
-  float y = GetScreenHeight() / 2.0f;
+  float y = (GetScreenHeight() / 2.0f) + (table::HEIGHT / 2.0f - table::OFFSET_Y / 2.0f) - 30; // 30 is the diameter of the ball
   float speed_x = 300.0f;
   float speed_y = 150.0f;
   float radius = 15;
-   
+
   void draw() {
     DrawCircle((int) x, (int) y, radius, WHITE);
   }
+
+  void reset() {
+    x = GetScreenWidth() / 2.0f;
+    y = (GetScreenHeight() / 2.0f) + (table::HEIGHT / 2.0f - table::OFFSET_Y / 2.0f);
+    speed_x = 300.0f;
+    speed_y = 150.0f;
+  }
 };
 
-/* ----- PALETTE -----
- * OOD FEELING PALETTE
+/* ----- OOD FEELING PALETTE -----
  * Creator: XENO
  * See: https://lospec.com/palette-list/odd-feeling
 */
--- src/main.cpp
+++ src/main.cpp
@@ -5,68 +5,34 @@ using namespace palette;
 int main() {
   InitWindow(screen::WIDTH, screen::HEIGHT, "Ping Pong"); // Initialize the window and opengl context
 
-  Image icon = LoadImage("resources/icon.png"); // Load image in memory
+  SetExitKey(KEY_NULL);
+
+  SetTargetFPS(60);
+
+  Image icon = LoadImage("resources/icon.png");
 
   SetWindowIcon(icon);
 
-  UnloadImage(icon); // Free the image (optimization)
+  UnloadImage(icon);
 
-  Texture background = LoadTexture("resources/background-pixel.png"); // Load the table
+  Texture layout = LoadTexture("resources/layout.png");
 
   int left_score = 0;
   int right_score = 0;
 
   Paddle left{
-    (GetScreenWidth() / 2.0f) - (table::WIDTH / 2) + 20, // 20 is a padding
-    GetScreenHeight() / 2.0f
+    (GetScreenWidth() / 2.0f) - (table::WIDTH / 2) + PADDLE_PADDING
   };
 
   Paddle right{
-    (GetScreenWidth() / 2.0f) + (table::WIDTH / 2) - 20, // -20 is a padding
-    GetScreenHeight() / 2.0f
+    (GetScreenWidth() / 2.0f) + (table::WIDTH / 2) - PADDLE_PADDING - 5 // 5 is border correction
   };
 
   Ball ball{};
 
-  SetTargetFPS(60);
-
   // If ESC is pressed the game closes
   while (!WindowShouldClose()) {
     /* --- UPDATE --- */
-
-    ball.x += ball.speed_x * GetFrameTime();
-    ball.y += ball.speed_y * GetFrameTime();
-
-    // ball limits
-    bool ball_ceil_limit = CheckCollisionCircleRec({ ball.x, ball.y },
-      ball.radius, { table::OFFSET_X, table::OFFSET_Y, table::WIDTH, 1 });
-
-    bool ball_floor_limit = CheckCollisionCircleRec({ ball.x, ball.y },
-      ball.radius, { table::OFFSET_X, table::HEIGHT + table::OFFSET_Y, table::WIDTH, 1 });
-
-    bool ball_left_limit = CheckCollisionCircleRec({ ball.x, ball.y },
-      ball.radius, { table::OFFSET_X, table::OFFSET_Y, 1, table::HEIGHT });
-
-    bool ball_right_limit = CheckCollisionCircleRec({ ball.x, ball.y },
-      ball.radius, { table::WIDTH + table::OFFSET_X, table::OFFSET_Y, 1, table::HEIGHT });
-
-    if (ball_ceil_limit) {
-      ball.speed_y *= -1;
-    }
-
-    if (ball_floor_limit) {
-      ball.speed_y *= -1;
-    }
-
-    if (ball_left_limit) {
-      ball.speed_x *= -1;
-      right_score += 1; // if the ball touches the left wall, right paddle points
-    }
-
-    if (ball_right_limit) {
-      ball.speed_x *= -1;
-      left_score += 1; // if the ball toucher the right wall, left paddle points
-    }
 
     // left paddle (wasd)
     if (IsKeyDown(KEY_S)) {
@@ -86,7 +52,14 @@ int main() {
       right.y -= PADDLE_SPEED * GetFrameTime();
     }
 
-    /* --- COLLISIONS paddle-BALL --- */
+    ball.x += ball.speed_x * GetFrameTime();
+    ball.y += ball.speed_y * GetFrameTime();
+
+    if (ball.speed_x > 2500) {
+      ball.speed_x = 300;
+    }
+
+    /* --- COLLISIONS paddle-ball --- */
     // left paddle-ball collision
     if (CheckCollisionCircleRec(
       { ball.x, ball.y },
@@ -94,8 +67,9 @@ int main() {
       { left.x, left.y, left.width, left.height }
     )) {
       if (ball.speed_x < 0) {
-        ball.speed_x *= -1.1f;
+        ball.speed_x *= -1.05f;
         ball.speed_y = (ball.y - left.y);
+        // SEE: https://gamedev.stackexchange.com/questions/4253/in-pong-how-do-you-calculate-the-balls-direction-when-it-bounces-off-the-paddl
       }
     }
 
@@ -105,10 +79,61 @@ int main() {
       ball.radius,
       { right.x, right.y, right.width, right.height }
     )) {
-      if (ball.speed_x < 0) {
-        ball.speed_x *= -1.1f;
+      if (ball.speed_x > 0) {
+        ball.speed_x *= -1.05f;
         ball.speed_y = (ball.y - right.y);
       }
+    }
+
+    // ball limits
+    bool ball_ceil_limit = CheckCollisionCircleRec(
+      { ball.x, ball.y },
+      ball.radius,
+      { table::OFFSET_X, table::OFFSET_Y, table::WIDTH, 1 }
+    );
+
+    bool ball_floor_limit = CheckCollisionCircleRec(
+      { ball.x, ball.y },
+      ball.radius,
+      { table::OFFSET_X, table::HEIGHT + table::OFFSET_Y - 20, table::WIDTH, 1 } // -20 is border correction
+    );
+
+    bool ball_left_limit = CheckCollisionCircleRec(
+      { ball.x, ball.y },
+      ball.radius,
+      { table::OFFSET_X, table::OFFSET_Y, 1, table::HEIGHT }
+    );
+
+    bool ball_right_limit = CheckCollisionCircleRec(
+      { ball.x, ball.y },
+      ball.radius,
+      { table::WIDTH + table::OFFSET_X - 15, table::OFFSET_Y, 1, table::HEIGHT } // -15 is border correction
+    );
+
+    if (ball_ceil_limit) {
+      ball.speed_y *= -1;
+    }
+
+    if (ball_floor_limit) {
+      ball.speed_y *= -1;
+    }
+
+    if (ball_left_limit) {
+      ball.reset();
+      ball.speed_x += ball.speed_x * GetFrameTime();
+      ball.speed_y += ball.speed_y * GetFrameTime();
+      right_score += 1 ; // if the ball touches the left wall, right paddle points
+    }
+
+    if (ball_right_limit) {
+      ball.reset();
+      ball.speed_x += ball.speed_x * GetFrameTime();
+      ball.speed_y += ball.speed_y * GetFrameTime();
+      left_score += 1; // if the ball toucher the right wall, left paddle points
+    }
+
+    if (IsKeyReleased(KEY_R)) {
+      ball.reset();
     }
 
     /* --- DRAW --- */
@@ -117,13 +142,13 @@ int main() {
     // Clear the screen before start drawing
     ClearBackground(RAYWHITE);
 
-    // Table
-    DrawTexture(background, 0, 0, WHITE);
+    // Table, players icon and scoreboard
+    DrawTexture(layout, 0, 0, WHITE);
 
     // Score board
     DrawText(TextFormat("%ix%i", left_score, right_score),
-      (screen::WIDTH / 2) - (MeasureText("xxx", 50) / 2),
-      35,
+      (screen::WIDTH - MeasureText("XxX", 50)) / 2,
+      screen::SCORE_OFFSET_Y,
       50,
       ODD_PURPLE
     );
@@ -137,15 +162,11 @@ int main() {
     // Ball
     ball.draw();
 
-    DrawFPS(50, 50);
-
     EndDrawing();
   }
 
-  // Unload background texture
-  UnloadTexture(background);
+  UnloadTexture(layout);
 
-  // Close window and unload opengl context
   CloseWindow();
 
   return 0;
